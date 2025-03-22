809698cee6efc2a5a77d1092ad89d3a4aa45e295
Sreedeep CV <sreedeep200@gmail.com>
2025-03-22 23:11:19 +0530

    corrected php

--- README.md
+++ README.md
@@ -6,3 +6,31 @@ cd intigriti-xxe-challenge
 php -S localhost:8000
 ```
 
+## challenge 
+![Challenge Image](./486066540_18220552186292613_50405887802685493_n.jpeg)
+
+## Solution 
+
+The code has a sanitation mechanism where common XML keywords are not allowed. 
+
+
+
+
+
+### About XXE
+
+XML external entity injection (also known as XXE) is a web security vulnerability that allows an attacker to interfere with an application's processing of XML data. 
+
+XML contains 
+
+
+Here is an example pyload: 
+```
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE foo [ <!ENTITY xxe SYSTEM "file:///etc/passwd"> ]>
+<stockCheck><productId>&xxe;</productId></stockCheck>
+```
+
+
+XXE can be futher expoited to perform a SSRF.
+
--- xml_parser.php
+++ xml_parser.php
@@ -1,14 +1,9 @@
 <?php
-libxml_disable_entity_loader(true); // Critical security setting
-
 function sanitize($in) {
-    // Remove DOCTYPE declarations
-    $in = preg_replace('/<!DOCTYPE[^>[]*(\[[^]]*\])?>/i', '', $in);
-    
-    $bad = ["<IENTITY", "<!ENTITY", " SYSTEM ", " PUBLIC "];
+    $bad = ["<!ENTITY>", "<!DOCTYPE>", "SYSTEM", "PUBLIC"];
     foreach ($bad as $kw) {
         if (stripos($in, $kw) !== false) {
-            throw new Exception("Forbidden XML structure detected!");
+            throw new Exception("Forbidden keyword!");
         }
     }
     return $in;
@@ -17,29 +12,21 @@ function sanitize($in) {
 function parse($xml) {
     $dom = new DOMDocument();
     $dom->loadXML($xml, LIBXML_NOENT | LIBXML_DTDLOAD);
-    
+
     $data = [];
     foreach ($dom->getElementsByTagName('data') as $e) {
         $data[] = [
             'title' => htmlspecialchars($e->getElementsByTagName('title')->item(0)->nodeValue),
-            'desc' => htmlspecialchars($e->getElementsByTagName('desc')->item(0)->nodeValue)
+            'desc'  => htmlspecialchars($e->getElementsByTagName('desc')->item(0)->nodeValue)
         ];
     }
     return $data;
 }
 
-// Handle POST request
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    try {
-        $xml = $_POST['data'] ?? '';
-        if (!empty($xml)) {
-            $result = parse(sanitize($xml));
-            echo "<h2>Parsed data:</h2><pre>";
-            print_r($result);
-        }
-    } catch (Exception $e) {
-        http_response_code(400);
-        echo "Error: " . $e->getMessage();
-    }
+$xml = $_POST['data'] ?? '';
+if (!empty($xml)) {
+    $result = parse(sanitize($xml));
+    echo "<h2>Parsed data:</h2><pre>";
+    print_r($result);
 }
 ?>
